66998439f50c1c240d9177f1ab90695f5fc7a93a
dharshini-anand <hellodharshu@live.com>
2021-07-16 09:45:31 -0700

    updated Database python file

--- cogface/Database.py
+++ cogface/Database.py
@@ -2,6 +2,9 @@ import numpy as np
 import pickle
 from facenet_models import FacenetModel
 from Profile import Profile
+from utils import cos_distance
+
+# from profile import Profile
 
 class Database:
      
@@ -12,14 +15,22 @@ class Database:
         
     # add image descriptor to profile, or create profile if it doesn't exist    
     def add_image(self, name, img):
+        if img.shape[-1] > 1080 or img.shape[-2] > 1080:
+            img = img[::4, ::4]
         boxes, probabilities, landmarks = self.model.detect(img)
-        descriptor = self.model.compute_descriptors(img, boxes)
-        if name in self.database:
-            self.database[name].add_descriptor(descriptor)
-        else:
-            profile = Profile(name)
-            profile.add_descriptor(descriptor)
-            self.database[name] = profile
+        idxs = []
+        for prob in probabilities:
+            if prob >= 0.98:
+                idxs.append(np.where(probabilities == prob)[0])
+        descriptor = self.model.compute_descriptors(img, boxes[idxs])
+        if descriptor.shape[0] == 1:
+            if name in self.database:
+                self.database[name].add_descriptor(descriptor)
+            else:
+                profile = Profile(name)
+                profile.add_descriptor(descriptor)
+                self.database[name] = profile
+            self.save_database()
      
     def remove_profile(self, name):
         self.database.pop(name)
@@ -32,6 +43,9 @@ class Database:
         with open("./database.pkl", mode="wb") as db:
             pickle.dump(self.database, db)
             
+    def create_database(self):
+        self.database = {}
+        
     # compare input to mean descriptors in database         
     def find_match(self, descriptor):
         #to change
@@ -44,6 +58,3 @@ class Database:
         if np.min(dists) < cutoff:
             return names[np.argmin(dists)]
         return "Unknown??"
-
-def cos_distance(descriptor_1, descriptor_2):
-    return 1 - descriptor_1 @ descriptor_2 / (np.linalg.norm(descriptor_1) * np.linalg.norm(descriptor_2))
